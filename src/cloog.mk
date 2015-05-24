0293191298987061d7f2807f785e7b87d9c20a21
Hamza Alloush <hamzaalloush@live.com>
2015-05-25 00:41:44 +0300

    revert gcc 4.9.3, without needed index.html modification

--- src/cloog.mk
+++ src/cloog.mk
@@ -1,20 +1,20 @@
 # This file is part of MXE.
 # See index.html for further information.
 
-PKG             := isl
+PKG             := cloog
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.12.2
-$(PKG)_CHECKSUM := ca98a91e35fb3ded10d080342065919764d6f928
+$(PKG)_VERSION  := 0.18.1
+$(PKG)_CHECKSUM := 2dc70313e8e2c6610b856d627bce9c9c3f848077
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
-$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
-$(PKG)_URL      := http://isl.gforge.inria.fr/$($(PKG)_FILE)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://www.bastoul.net/cloog/pages/download/$($(PKG)_FILE)
 $(PKG)_URL_2    := ftp://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc gmp
+$(PKG)_DEPS     := gcc gmp isl
 
 # stick to tested versions from gcc
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'ftp://gcc.gnu.org/pub/gcc/infrastructure/' | \
-    $(SED) -n 's,.*isl-\([0-9][^>]*\)\.tar.*,\1,p' | \
+    $(SED) -n 's,.*cloog-\([0-9][^>]*\)\.tar.*,\1,p' | \
     $(SORT) -V |
     tail -1
 endef
@@ -25,7 +25,8 @@ define $(PKG)_BUILD
         --enable-static \
         --disable-shared \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --with-gmp-prefix='$(PREFIX)/$(TARGET)'
+        --with-gmp-prefix='$(PREFIX)/$(TARGET)' \
+        --with-isl-prefix='$(PREFIX)/$(TARGET)'
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j '$(JOBS)' install
 endef
