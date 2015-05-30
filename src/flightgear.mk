348798b955344da3184825c41b21100a6be38bc5
Saikrishna Arcot <saiarcot895@gmail.com>
2015-05-30 08:20:15 -0700

    Add build script for Flightgear.
    Flightgear now builds, but doesn't run due to missing SImgear libraries.

---
+++ src/flightgear.mk
@@ -0,0 +1,36 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := flightgear
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 3.4.0
+$(PKG)_CHECKSUM := b919f3e1d6f62439a5ef376c11766657a66ee6c8
+$(PKG)_SUBDIR   := flightgear-$($(PKG)_VERSION)
+$(PKG)_FILE     := flightgear-$($(PKG)_VERSION).tar.bz2
+$(PKG)_URL      := http://download.flightgear.org/flightgear/Source/$($(PKG)_FILE)
+$(PKG)_DEPS     := gcc cmake simgear openscenegraph jpeg libpng plib boost openal freeglut sqlite
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://download.flightgear.org/flightgear/Source/' | \
+    $(SED) -n 's,.*flightgear-\([0-9]*\.[0-9]*[02468]\.[^<]*\)\.tar.*,\1,p' | \
+    grep -v rc | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake '$(1)' \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DCMAKE_CXX_FLAGS='-D__STDC_CONSTANT_MACROS -D_USE_MATH_DEFINES -fpermissive' \
+        -DCMAKE_HAVE_PTHREAD_H=OFF \
+        -DCMAKE_VERBOSE_MAKEFILE=ON \
+        -DPKG_CONFIG_EXECUTABLE='$(PREFIX)/bin/$(TARGET)-pkg-config' \
+		 $(if $(BUILD_STATIC), \
+		 -DSIMGEAR_SHARED=OFF, \
+		 -DSIMGEAR_SHARED=ON) \
+		-DSYSTEM_SQLITE=ON \
+		-DENABLE_FGCOM=OFF \
+		-DENABLE_IAX=OFF
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
+endef
