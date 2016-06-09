98476dd64c270dad29cb93e70f717e7be1350c2b
Saikrishna Arcot <saiarcot895@gmail.com>
2016-06-08 20:56:38 -0500

    Simgear now compiles with these fixes.

--- src/simgear.mk
+++ src/simgear.mk
@@ -20,13 +20,14 @@ define $(PKG)_BUILD
     mkdir '$(1).build'
     cd '$(1).build' && cmake '$(1)' \
         -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
-        -DCMAKE_CXX_FLAGS='-D_USE_MATH_DEFINES -std=c++11 -DOSG_LIBRARY_STATIC' \
         -DCMAKE_VERBOSE_MAKEFILE=ON \
         -DPKG_CONFIG_EXECUTABLE='$(PREFIX)/bin/$(TARGET)-pkg-config' \
 		 $(if $(BUILD_STATIC), \
+         -DCMAKE_CXX_FLAGS='-D_USE_MATH_DEFINES -std=c++11 -DOSG_LIBRARY_STATIC' \
 		 -DSIMGEAR_SHARED=OFF \
 		 -DSYSTEM_EXPAT=OFF \
 		 -DBoost_USE_STATIC_LIBS=ON, \
+         -DCMAKE_CXX_FLAGS='-D_USE_MATH_DEFINES -std=c++11' \
 		 -DSIMGEAR_SHARED=ON \
 		 -DSYSTEM_EXPAT=ON)
     $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
